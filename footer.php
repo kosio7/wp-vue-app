da939b8cdacfa3464eb894422990f621628e87f9
konstantin.stefanov <konstantin.stefanov@mobilewaves.com>
2018-09-16 19:09:58 +0300

    Adds style to the posts using Vuetify

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/vue-resource.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/lib/moment.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/App.js"></script>	
+	<script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,7 +8,6 @@
   <template id='posts-template'>    
     <div>
       <v-toolbar>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title>Post Fetcher</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
@@ -17,17 +16,35 @@
         </v-toolbar-items>
       </v-toolbar>
       <div v-for='post in posts'>
-        {{ moment(post.date).format('MMMM Do YYYY, h:mm:ss a') }}
-        {{ post.title.rendered }}
-        {{ post._embedded.author[0].name }}
-      </div>
-    </div> 
+        <v-card class="my-3">        
+          <v-container>
+            <v-layout>
+              <v-flex d-flex>
+                <span class="headline">{{ post.title.rendered }}</span>                
+              </v-flex>
+            </v-layout>
+            <v-layout>              
+              Article written by &nbsp
+              <p class='author'>
+                {{ post._embedded.author[0].name }}
+                <!-- NOTE: Find a way to present background image for the post. -->
+                <!-- {{ post._embedded['wp:featuredmedia'][0] }} -->
+              </p>
+            </v-layout>
+            <v-layout>          
+              Post added on:
+              {{ moment(post.date).format('MMMM Do YYYY, h:mm a') }}              
+            </v-layout>
+          </v-container>
+            </div>
+          </div> 
+        </v-card>
+        
   </template>
 
   <template id='articles-template'>
     <div>
     <v-toolbar>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title>Post Fetcher</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
@@ -40,4 +57,12 @@
     </div>
     </div> 
   </template>
+
+  <style>  
+    .author {
+    font-family: Verdana, Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    color: #383333;
+  }
+  </style>
 <?php get_footer(); ?>
